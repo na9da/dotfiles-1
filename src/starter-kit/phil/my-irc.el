f34a45d557ae29f70f67d9680379be82ae630df3
Phil Hagelberg <technomancy@gmail.com>
2010-12-08 20:30:28 -0800

    Lajla is a tool.

--- src/starter-kit/phil/my-irc.el
+++ src/starter-kit/phil/my-irc.el
@@ -2,23 +2,28 @@
   '(progn
      (setq erc-prompt ">"
            erc-fill-column 75
+           erc-max-buffer-size 100000
            erc-hide-list '("JOIN" "PART" "QUIT" "NICK")
            erc-track-exclude-types (append '("324" "329" "332" "333"
                                              "353" "477" "MODE")
                                            erc-hide-list)
            erc-nick '("technomancy" "TeXnomancy")
-           erc-ignore-listerc-ignore-list '("Lajla")
            erc-autojoin-timing :ident
            erc-flood-protect nil
            erc-autojoin-channels-alist
            '(("freenode.net" "#emacs" "#clojure" "#seattle.rb" "#leiningen"
               "#sonian" "#sonian-safe" "#mirah"))
            erc-prompt-for-nickserv-password nil)
+
+     (setq-default erc-ignore-list '("Lajla")
+                   erc-ignore-reply-list '("Lajla"))
      (require 'erc-services)
      (require 'erc-spelling)
+     (require 'erc-truncate)
      (erc-services-mode 1)
      (add-to-list 'erc-modules 'highlight-nicknames 'spelling)
      (add-hook 'erc-connect-pre-hook (lambda (x) (erc-update-modules)))
+     (add-hook 'erc-insert-post-hook 'erc-truncate-buffer)
      (set-face-foreground 'erc-input-face "dim gray")
      (set-face-foreground 'erc-my-nick-face "blue")))
 
