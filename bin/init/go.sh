30d724893119a303c02ba56c4886f8e9ad8f08eb
Phil Hagelberg <technomancy@gmail.com>
2011-03-15 10:41:47 -0700

    system init updates

--- bin/init/go.sh
+++ bin/init/go.sh
@@ -10,7 +10,7 @@ add-apt-repository ppa:launchpad/ppa
 add-apt-repository ppa:openjdk/ppa
 
 # grrr... vagrant doesn't work with regular virtualbox; needs Oracle's.
-echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) non-free" > /etc/apt/sources.list.d/virtualbox.list
+echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" > /etc/apt/sources.list.d/virtualbox.list
 wget -O /etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list 
 
 sudo apt-get --quiet update
