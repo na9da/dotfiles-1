6142f01b37660f4c8e8d897bac6593e38c3b8817
Phil Hagelberg <technomancy@gmail.com>
2011-05-27 11:27:21 -0700

    Make emacs init.el compatible with 23.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,26 +1,28 @@
 ;;; My general personalizations outside the starter kit
 
-;; Clojure confluence
-
-(setq confluence-url "http://dev.clojure.org/confluence/rpc/xmlrpc")
-(global-set-key "\C-xwf" 'confluence-get-page)
-
 ;; Random stuff
 
 (setq-default save-place t)
-(setq whitespace-line-column 80)
 
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "conkeror")
 
-(org-remember-insinuate)
-
-(global-set-key (kbd "C-c C-r") 'remember)
 (global-set-key (kbd "C-c q") 'join-line)
 
-(ignore-errors
-  (load "~/src/safe/.elisp/sonian-navigation.el"))
+(ignore-errors (load "~/src/safe/.elisp/sonian.el"))
+
+;; Packages
+
+(when (not (require 'package nil t))
+  (load-file "/home/phil/.emacs.d/package-23.el"))
+
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(package-initialize)
+
+;; why not?
 
 (eshell)
-;; graaaaaaah!
+;; graaaaaaah! eshell doesn't respect eval-after-load for some reason:
 (with-current-buffer "*eshell*" (setq pcomplete-cycle-completions nil))
