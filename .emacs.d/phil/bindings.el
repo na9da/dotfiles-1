1b79359403ff665c1fd3f910b04bdfb6365ef587
Phil Hagelberg <technomancy@gmail.com>
2011-06-29 11:09:54 -0700

    Add some scripts and stuff moved out of the starter kit.

---
+++ .emacs.d/phil/bindings.el
@@ -0,0 +1,5 @@
+;; Start eshell or switch to it if it's active.
+(global-set-key (kbd "C-x m") 'eshell)
+
+;; Start a new eshell even if one is active.
+(global-set-key (kbd "C-x M") (lambda () (interactive) (eshell t)))
---
+++ .emacs.d/phil/misc.el
@@ -0,0 +1,4 @@
+(add-hook 'oddmuse-mode-hook
+          (lambda ()
+            (unless (string-match "question" oddmuse-post)
+              (setq oddmuse-post (concat "uihnscuskc=1;" oddmuse-post)))))
