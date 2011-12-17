8175fc020f3b259e742ea54adde6952f4795b6a9
Phil Hagelberg <phil@enigma.(none)>
2011-12-17 13:48:28 -0800

    Bootstrapping fixes.

--- bin/init/go.sh
+++ bin/init/go.sh
@@ -14,19 +14,23 @@ echo "APT::Install-Recommends \"0\";" > /etc/apt/apt.conf.d/50norecommends
 apt-get update
 apt-get install -y git zile ruby1.9.1
 
-if [ ! -r ~/.dotfiles ]; then
+export ME=phil
+getent passwd phil || \
+	(export ME=technomancy && getent passwd technomancy || \
+	 export ME=vagrant)
+
+usermod -a -G sudo $ME
+
+if [ ! -r /home/$ME/.dotfiles ]; then
   echo "Checking out dotfiles..."
-  if [ ssh-add -l ]; then
-    DOTFILES_URL=git@github.com:technomancy/dotfiles.git
-  else
+  if [ "`ssh-add -l`" = "The agent has no identities." ]; then
     DOTFILES_URL=git://github.com/technomancy/dotfiles.git
+  else
+    DOTFILES_URL=git@github.com:technomancy/dotfiles.git
   fi
-  ME=phil
-  getent passwd phil || ME=technomancy
-  getent passwd technomancy || ME=vagrant
-  sudo -u $ME git clone $DOTFILES_URL ~/.dotfiles
+  sudo -u $ME git clone $DOTFILES_URL /home/$ME/.dotfiles
 fi
 
-~/.dotfiles/bin/link-dotfiles
+sudo -u $ME /home/$ME/.dotfiles/bin/link-dotfiles
 
-exec ~/.dotfiles/bin/install.sh
+exec /home/$ME/.dotfiles/bin/init/install.sh
--- bin/init/install.sh
+++ bin/init/install.sh
@@ -9,16 +9,16 @@ chattr +A /
 
 echo "Installing packages via apt-get and rubygems..."
 
-cd ~/bin/init
-apt-get install $(ruby1.9.1 -ryaml -e "puts YAML.load_file('debs.yml').join ' '")
+cd /home/$ME/bin/init
+apt-get install -y $(ruby1.9.1 -ryaml -e "puts YAML.load_file('debs.yml').join ' '")
 
 if [ "$DISPLAY" != "" ] ; then
-  apt-get install $(ruby1.9.1 -ryaml -e "puts YAML.load_file('gui-debs.yml').join ' '")
-  gem install $(ruby1.9.1 -ryaml -e "puts YAML.load_file('gems.yml').join ' '")
+  apt-get install -y $(ruby1.9.1 -ryaml -e "puts YAML.load_file('gui-debs.yml').join ' '")
+  gem1.9.1 install --no-rdoc --no-ri $(ruby1.9.1 -ryaml -e "puts YAML.load_file('gems.yml').join ' '")
   cp xsession.desktop /usr/share/xsessions/xsession.desktop
 fi
 
 echo "All done! Happy hacking."
 
-# TODO: support dyndns
+# TODO: support dyndns on pair.io
 # wget -q "http://$DYNUSER:$DYNPASS@members.dyndns.org/nic/update?hostname=enigma.dyn-o-saur.com&myip=$IP&wildcard=NOCHG&mx=NOCHG&backmx=NOCHG"
