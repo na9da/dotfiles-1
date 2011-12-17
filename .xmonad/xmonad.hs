dab5faff7df4345ac8a367b7ea41f2d682b7bef2
Phil Hagelberg <technomancy@gmail.com>
2011-12-16 17:08:57 -0800

    Simplify xmonad config.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,7 +3,4 @@ import XMonad.Config.Gnome
 
 -- TODO: fix erythrina to act as a panel
 main = xmonad gnomeConfig
-         { modMask = mod4Mask
-         , terminal = "gnome-terminal"
-         , focusFollowsMouse = False
-         }
+       { modMask = mod4Mask }
