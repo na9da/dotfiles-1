589d8c97a15b8c8753cb6b7de40d576fcdc8786b
Phil Hagelberg <technomancy@gmail.com>
2012-01-04 20:58:14 -0800

    Switch from erc back to rcirc.

--- .emacs.d/phil/my-irc.el
+++ .emacs.d/phil/my-irc.el
@@ -1,62 +1,30 @@
-(eval-after-load 'erc
-  '(progn
-     (when (not (package-installed-p 'erc-hl-nicks))
-       (package-install 'erc-hl-nicks))
-     (setq erc-prompt ">"
-           erc-server-reconnect-timeout 30
-           erc-fill-column 75
-           erc-max-buffer-size 100000
-           erc-hide-list '("JOIN" "PART" "QUIT" "NICK")
-           erc-track-exclude-types (append '("324" "329" "332" "333"
-                                             "353" "477" "MODE")
-                                           erc-hide-list)
-           erc-nick '("technomancy" "TeXnomancy")
-           erc-autojoin-timing :ident
-           erc-flood-protect nil
-           erc-autojoin-channels-alist
-           '(("freenode.net" "#emacs" "#clojure" "#leiningen"))
-           erc-prompt-for-nickserv-password nil)
-
-     (setq-default erc-ignore-list '("Lajla" "pjb" "e1f"))
-     (require 'erc-services)
-     (require 'erc-spelling)
-     (require 'erc-truncate)
-     (require 'erc-hl-nicks)
-     (erc-services-mode 1)
-     (add-to-list 'erc-modules 'hl-nicks 'spelling)
-     (add-hook 'erc-connect-pre-hook (lambda (x) (erc-update-modules)))
-     (add-hook 'erc-insert-post-hook 'erc-truncate-buffer)
-     (set-face-foreground 'erc-input-face "dim gray")
-     (set-face-foreground 'erc-my-nick-face "blue")))
-
-(setq pcomplete-cycle-completions nil)
+;;; trying out rcirc
 
 (ignore-errors
-  (load (expand-file-name "~/.chorts.el"))
+  (load (expand-file-name "~/.chorts.el")))
 
-  (setq erc-nickserv-passwords
-        `((freenode (("technomancy" . ,freenode)
-                     ("TeXnomancy" . ,freenode))))))
+(setq rcirc-server-alist `(("route.heroku.com" :port 10688
+                            :encryption tls :nick "technomancy"
+                            :password ,znc-password
+                            :channels ("#emacs" "#clojure" "#leiningen"
+                                       "#xmonad" "#84115" "#seajure")))
+      rcirc-ignore-list '("pjb" "e1f" "Lajla")
+      rcirc-fill-column 75
+      rcirc-buffer-maximum-lines 5000
+      rcirc-authinfo `(("freenode" nickserv "technomancy" ,freenode-password)
+                       ("heroku" nickserv "technomancy" ,freenode-password)
+                       ("freenode" nickserv "TeXnomancy" ,freenode-password)))
 
-(defun clean-message (s)
-  (let* ((s (replace-regexp-in-string ">" "&gt;" s))
-         (s (replace-regexp-in-string "<" "&lt;" s))
-         (s (replace-regexp-in-string "&" "&amp;" s))
-         (s (replace-regexp-in-string "\"" "&quot;" s))))
-  (replace-regexp-in-string "'" "&apos;" s))
+(add-hook 'rcirc-mode-hook 'turn-on-flyspell)
+(add-hook 'rcirc-mode-hook 'rcirc-omit-mode)
 
-(defun call-libnotify (matched-type nick msg)
-  (ignore-errors
-    (let* ((cmsg  (split-string (clean-message msg)))
-           (nick   (first (split-string nick "!")))
-           (msg    (mapconcat 'identity (rest cmsg) " ")))
-      (shell-command-to-string
-       (format "notify-send -i %s/%s '%s says:' '%s'"
-               "/home/phil/src/emacs"
-               "etc/images/icons/hicolor/scalable/apps/emacs.svg"
-               nick msg)))))
+(eval-after-load 'rcirc
+  '(dolist (p '(rcirc-notify rcirc-color rcirc-ucomplete))
+     (when (not (package-installed-p p))
+       (package-install p))))
 
-(add-hook 'erc-text-matched-hook 'call-libnotify)
+(eval-after-load 'rcirc
+  '(rcirc-track-minor-mode t))
 
 (defun window-register-bottom (r &optional x)
   (interactive "cJump to register: \nP")
@@ -64,24 +32,3 @@
   (walk-windows (lambda (w) (end-of-buffer))))
 
 (global-set-key (kbd "C-x w") 'window-register-bottom)
-
-(defun campervan ()
-  (interactive)
-  (erc :server "localhost" :port 6667
-       :nick "phil_hagelberg" :password campervan))
-
-;; For some reason, campervan doesn't honor autojoin
-(defun campervan-join ()
-  (interactive)
-  (dolist (channel '("#heroku_dev_lounge" "#clojure" "#java_team" "#packaging"))
-    (erc-join-channel channel)))
-
-(defun znc ()
-  (interactive)
-  (require 'erc)
-  (erc-spelling-enable)
-  (erc-tls :server "route.heroku.com" :port 10688 :nick "technomancy"
-           :password (concat "technomancy:" freenode)))
-
-(autoload 'erc-tls "erc" "" t)
-(autoload 'erc-ssl "erc" "" t)
