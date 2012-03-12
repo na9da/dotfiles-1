e2e3b5ae55d2696b7a9f41bdb3503c51a445f1ea
Phil Hagelberg <technomancy@gmail.com>
2012-03-12 11:24:22 -0700

    Get rid of the stupid red border on full-screened xmonad windows.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,16 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig (additionalKeys)
 
 main = xmonad $ gnomeConfig
-       { modMask = mod4Mask
-       , focusFollowsMouse = False -- trackpoint jitter makes this unusable
+       {
+       -- seriously? you want to steal meta?
+       modMask = mod4Mask
+       -- red border is useless on full-screened windows
+       , layoutHook = smartBorders (layoutHook defaultConfig)
+       -- trackpoint jitter makes this unusable
+       , focusFollowsMouse = False
        }
+       -- gnome's launcher is crappy compared to dmenu
        `additionalKeys` [ ((mod4Mask, xK_p), spawn "dmenu_run") ]
