5266b5f95761202f6b2849cfea4138633e2d09d1
Phil Hagelberg <technomancy@gmail.com>
2012-03-12 11:35:21 -0700

    Lite init script should work on systems that lack wget.

--- bin/init/lite.sh
+++ bin/init/lite.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
+# Bootstrap a smaller set of dotfiles; should be more cross-platform.
+
+set -e
+
 dotfiles_raw=https://github.com/technomancy/dotfiles/raw/master
 
+if type -p curl >/dev/null 2>&1; then
+    HTTP_CLIENT="curl -f -L -o"
+else
+    HTTP_CLIENT="wget -O"
+fi
+
 mkdir -p ~/.emacs.d
-wget -O ~/.emacs.d/init.el $dotfiles_raw/.emacs.d/init-lite.el
+$HTTP_CLIENT -O ~/.emacs.d/init.el $dotfiles_raw/.emacs.d/init-lite.el
 
 files=".bashrc .bash_aliases .profile .tmux.conf"
 
 cd ~
 for f in $files; do
-    wget $dotfiles_raw/$f
+    $HTTP_CLIENT $dotfiles_raw/$f
 done
 
 git config --global user.name "Phil Hagelberg"
 git config --global user.email "technomancy@gmail.com"
 
+# run as user
 # grab elisp packages
 emacs --batch -l .emacs.d/init.el -f kill-emacs
 
 source .bashrc
+
+# not being honored
+export TERM=xterm-256color
 
 which tmux-shared && tmux-shared
