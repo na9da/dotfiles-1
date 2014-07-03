5505bab837a73f6d77c5e6b20d0178792b691c51
Phil Hagelberg <technomancy@gmail.com>
2014-07-02 19:54:58 -0700

    Bunch of other random stuff.

--- .offlineimap.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import re, os
-
-def get_authinfo_password(machine, login):
-    s = "machine %s login %s password ([^ ]*)\n" % (machine, login)
-    p = re.compile(s)
-    authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
-    return p.search(authinfo).group(1)
