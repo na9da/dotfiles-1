810987847ad5cb97e45619216ff1dac5a92c9458
Phil Hagelberg <phil@hagelb.org>
2017-10-18 10:50:15 -0700

    Such good exwms.

--- .emacs.d/phil/wm.el
+++ .emacs.d/phil/wm.el
@@ -22,17 +22,58 @@
                ("s-v" "killall evrouter; evrouter /dev/input/*")
                ("s-s" "scrot")
                ("s-S-s" "scrot -s")
+               ("s-<return>" "urxvt")
                ("<f7>" "music-choose")
                ("S-<f7>" "music-random")
                ("<f8>" "mpc toggle")
                ("<f10>" "mpc next")
                ("<XF86AudioLowerVolume>" "amixer sset Master 5%-")
-               ("<XF86AudioRaiseVolume>" "amixer sset Master 5%+")))
-    (global-set-key (kbd (car k)) (lambda () (interactive)
-                                    (save-window-excursion
-                                      (shell-command (concat (cadr k) " &"))))))
+               ("<XF86AudioRaiseVolume>" (concat "amixer set Master unmute; "
+                                                 "amixer sset Master 5%+"))))
+    (let ((f (lambda () (interactive)
+               (save-window-excursion
+                 (start-process-shell-command (cadr k) nil (cadr k))))))
+      (exwm-input-set-key (kbd (car k)) f)))
+
+  (defun pnh-run (command)
+    (interactive (list (read-shell-command "$ ")))
+    (start-process-shell-command command nil command))
+  (define-key exwm-mode-map (kbd "C-x s-m") 'pnh-run)
+  (global-set-key (kbd "C-x s-m") 'pnh-run)
+
+  (exwm-input-set-simulation-keys
+   (mapcar (lambda (c) (cons (kbd (car c)) (cdr c)))
+           `(("C-b" . left)
+             ("C-f" . right)
+             ("C-p" . up)
+             ("C-n" . down)
+             ("C-a" . home)
+             ("C-e" . end)
+             ("M-v" . prior)
+             ("C-v" . next)
+             ("C-d" . delete)
+             ("C-m" . return)
+             ("C-i" . tab)
+             ("C-g" . escape)
+             ("C-s" . ?\C-f)
+             ("C-y" . ?\C-v)
+             ("M-w" . ?\C-c)
+             ("M-<" . C-home)
+             ("M->" . C-end)
+             ("C-M-h" . C-backspace))))
 
   (when window-system
+    (when (string= system-name "alto")
+      (require 'exwm-randr)
+      (setq exwm-randr-workspace-output-plist '(2 "eDP-1" 3 "DP-1" 1 "HDMI-2"
+                                                  4 "DP-1" 5 "DP-1" 6 "DP-1"
+                                                  7 "eDP-1" 8 "eDP-1" 9 "eDP-1"
+                                                  0 "HDMI-2"))
+      (add-hook 'exwm-randr-screen-change-hook
+                (lambda ()
+                  (start-process-shell-command "xrandr" nil "~/bin/rotated")))
+      (exwm-randr-enable))
+
     (global-set-key (kbd "C-x m")
                     (defun pnh-eshell-per-workspace (n)
                       (interactive "p")
@@ -43,14 +84,10 @@
   ;; todo:
 
   ;; * switch to char-mode in urxvt
-
-  ;; (require 'exwm-randr)
-  ;; (setq exwm-randr-workspace-output-plist '(0 "VGA1"))
-  ;; (add-hook 'exwm-randr-screen-change-hook
-  ;;           (lambda ()
-  ;;             (start-process-shell-command
-  ;;              "xrandr" nil "xrandr --output VGA1 --left-of LVDS1 --auto")))
-  ;; (exwm-randr-enable)
+  ;; * how to send C-f in simulation keys
+  ;; * figure out how to recover "lost" floating windows
+  ;; * allow global-set-key stuff in browser/urxvt
+  ;; * sometimes gpg passphrase entry doesn't accept input
 
   ;; cheat sheet:
   ;; * C-c C-k: switch to char-mode
